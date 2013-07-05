4d363d9f453e8bfa1548ee0877fe360e8fc471ff
Andrew Parmar <andrew.parmar@gmail.com>
2013-07-05 16:22:49 +0000

    Using index.html for string

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = requie('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, encoding));
+  response.send(fs.readFileSync(index.html, 'utf8'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
